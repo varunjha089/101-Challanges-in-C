db91e1a7e675bb9c5e6ed5f8fd3b88ca415e9de1
VARUN KUMAR <varunjha089@gmail.com>
2023-01-04 17:22:43 +0000

    Writing delete function of Linked List.

---
+++ ch_11/88_linked_list.c
@@ -0,0 +1,44 @@
+/*Linked List Data Structure*/
+#include <stdio.h>
+#include <malloc.h>
+
+/*Structure of linked list contains link part and data part*/
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void append(struct node **, int);
+void addAtBeg(struct node **, int);
+void addAfter(struct node *, int, int);
+void display(struct node *);
+int count(struct node *);
+void del(struct node **, int);
+
+int main(){
+
+}
+
+void append(struct node **q, int num){}
+
+void addAtBeg(struct node **q, int num){}
+
+void addAfter(struct node *q, int loc, int num){}
+
+void display(struct node *q){}
+
+int count(struct node *q){}
+
+void del(struct node **q, int num){
+    struct node *old, *temp;
+
+    temp = *q;
+
+    while(temp != NULL){
+        if(temp -> data == num){
+            /*If node to be deleted is the first node of the Linked List*/
+            
+        }
+    }
+}
